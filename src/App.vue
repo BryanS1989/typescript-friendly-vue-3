f5faf81d444c0bdb15523e5260ba578519d9b7ec
BryanS1989 <bryanstevenal@gmail.om>
2024-05-11 21:10:44 +0200

    Typing Your Callback Functions

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { ref, reactive, onMounted } from "vue";
+import { ref, reactive, onMounted, computed } from "vue";
 import fetchCount from "./fetchCount";
 
 interface AppInfo {
@@ -21,6 +21,22 @@ interface AppInfo {
 */
 const count = ref<number | null>(null);
 
+/*
+  In this case, the type could be a number (count.value is a number)
+  and if we +1 still a number.
+  But we also return null then the type inferred will be:
+    
+    ComputedRef<number | null>
+
+const nextCount = computed(() => {
+  if (count.value !== null) {
+    return count.value + 1;
+  }
+
+  return null;
+});
+*/
+
 const appInfo : AppInfo = reactive({
   name: 'Counter',
   slogan: 'an app you can count on'
@@ -31,6 +47,12 @@ onMounted(() => {
     count.value = initialCount;
   });
 });
+
+function addCount (num: number) {
+  if (count.value !== null) {
+    count.value += num;
+  }
+}
 </script>
 
 <template>
@@ -49,6 +71,10 @@ onMounted(() => {
   </header>
 
   <p>{{ count }}</p>
+
+  <p>
+    <button @click="addCount(1)">Add</button>
+  </p>
 </template>
 
 <style scoped>
